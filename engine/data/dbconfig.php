29c64f131660080f6888114e30c6f618de41cc72
Mirzoxid <33898124+Mirzoxid@users.noreply.github.com>
2018-01-13 16:19:41 +0500

    Update dbconfig.php

--- engine/data/dbconfig.php
+++ engine/data/dbconfig.php
@@ -1,21 +1,11 @@
 <?PHP
-
-define ("DBHOST", "localhost"); 
-
-define ("DBNAME", "gals");
-
-define ("DBUSER", "root");
-
-define ("DBPASS", "");  
-
+define ("DBHOST", "hrr5mwqn9zs54rg4.chr7pe7iynqr.eu-west-1.rds.amazonaws.com"); 
+define ("DBNAME", "xvtcqos9cg3eph3o");
+define ("DBUSER", "ah4zau4bp4b7yqzp");
+define ("DBPASS", "wq8uc01mbgpwtzm4");  
 define ("PREFIX", "dle");
-
 define ("USERPREFIX", "dle");
-
 define ("COLLATE", "cp1251");
-
 define('SECURE_AUTH_KEY', '8o- JSDUf9fL.sx|8Mi,~@&h4/ |/lu4^*L|gue/[5eNfHfsc}{JOG J@E[[N;');
-
 $db = new db;
-
 ?>
